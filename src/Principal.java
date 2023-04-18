ad4d660b65ccf5247ef76fa7c3be17a4f09bf7d9
10722128072 <10722128072@UNS11DT099724.animaedu.intranet>
2023-04-18 20:11:08 -0300

    primeiro commit

---
+++ src/Principal.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+import java.net.*;
+import java.io.*;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket servidor = new ServerSocket(4444);
+            System.out.println("Aguardando cliente!");
+            while (true) {
+                Socket socket = servidor.accept(); // espera
+                Servidor cliente = new Servidor(socket);
+                System.out.println("Chegou um cliente!");
+                cliente.start();
+            }
+        } catch (IOException e) {
+            System.err.println("Problemas de IO");
+        }
+
+    }
+}
